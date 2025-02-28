70551a9d512f9825938cb6e84fe49a287ade7e3a
Jay-Asalways <>
2025-02-28 13:24:10 +0700

    feat: commit exercises

--- ex-5.js
+++ ex-5.js
@@ -2,3 +2,7 @@
 let companyName = "TechUp";
 
 // Start coding here
+for(let i = 0; i < companyName.length; i++) {
+    let number = i +1 
+    console.log("Number " + number + " character is " + companyName[i]);
+}
