9e83f49452a72969fe2b6bc299cd6a59b167dbcd
Wutt Saeng <wuttforwork@gmail.com>
2025-02-28 14:18:31 +0700

    ADD: Loop Exercise

--- ex-1.js
+++ ex-1.js
@@ -1,4 +1,9 @@
 // Exercise #1: Decrease the Student Scores
 let scores = [89, 70, 60, 50, 100, 71];
-
+let newScores = 0;
 // Start coding here
+for (let i = 0; i < scores.length; i++) {
+    newScores < scores
+    newScores = scores[i] - 10;
+    console.log(newScores);
+}
--- ex-5.js
+++ ex-5.js
@@ -1,4 +1,8 @@
 // Exercise #5: Concatenate the Alphabets
 let companyName = "TechUp";
-
+let count = 0;
 // Start coding here
+for (let x = 0; x < companyName.length; x++) {
+     console.log(`Number ${x+1} character: ${companyName[x]}`);
+    
+}
