f6ad8c40c91438a82848721afeb0683386385f16
Binwaran <binwaran.m@gmail.com>
2025-02-28 14:29:38 +0800

    Add: Exercise 1-7

--- ex-1.js
+++ ex-1.js
@@ -1,4 +1,10 @@
 // Exercise #1: Decrease the Student Scores
 let scores = [89, 70, 60, 50, 100, 71];
+let newScores = [];
 
 // Start coding here
+for (let i = 0; i < scores.length; i++) {
+    scores[i] -= 10;
+    newScores.push(scores[i]);
+}
+console.log(newScores);
--- ex-4.js
+++ ex-4.js
@@ -2,5 +2,10 @@
 let restaurants = ["KFC", "MOMO PARADISE", "Nabezo"];
 let newRestaurants = [];
 // Start coding here
+for (let i = 0; i < restaurants.length; i++) {
+    restaurants[i] = "Restaurant Name: " + restaurants[i]; {
+        newRestaurants.push(restaurants[i])
+    }
+}
 
 console.log(newRestaurants);
